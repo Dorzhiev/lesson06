9d4ba23db367fc3546ef514d61ad075e5cc76c13
Ayur Dorzhiev <ayurra@mail.ru>
2021-01-13 15:24:23 +0300

    бот, игра угадай число с 3х попыток от 0 до 100

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
 'use strict';
-
 let isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n)
-}
-
-
-
-
-let correct = Math.floor(Math.random()*101);
-console.log(correct);
-
+  return !isNaN(parseFloat(n)) && isFinite(n); //пропускает только числа
+};
 function guessNumber(){
-    let namber = prompt('Угадай число от 1 до 100', correct);
+    let correct = Math.floor(Math.random()*101);
+    console.log(correct);
+
+    let count = 3; //попытки
+    
+    return function add(){ 
+    if(count < 0) { //тут надо подумать, попытки на один больше получается 
+        let con = confirm('Попытки закончились, загаданное число было ' + correct + ', повторим игру?');
+        if(con) {
+            guessNumber();
+            count = 3;
+            add();  
+        } else {
+            return;
+        }
+    }  
+    let namber = prompt('Угадай число от 1 до 100');
     if (namber === null) {    
         return alert('Игра окончена');
-    };
-    if (namber ==='' || !isNumber(namber)) {    
-        alert('Введи число!');
-        guessNumber();
-    };
-    
-    if (namber > correct) {    
-         alert('Загаданное число меньше');
-         guessNumber();
+    } else if (namber ==='' || !isNumber(namber)) { 
+        alert('Введи число! Осталось ' + count + ' попыток.');
+        count--;
+        add();
+    } else if (namber > correct) {    
+         alert('Загаданное число меньше! Осталось ' + count + ' попыток.');
+         count--;
+         add();
 
-    };
-    if (namber < correct) {    
-        alert('Загаданное число больше');
-        guessNumber();
-    };
-    if (namber == correct) {    
-         alert('Поздравляю, Вы угадали!!!');
-    };
+    } else if (namber < correct) {    
+        alert('Загаданное число больше! Осталось ' + count + ' попыток.');
+        count--;
+        add();
+    } else if (namber == correct) {    
+        alert('Поздравляю, Вы угадали!!!');
+    }
+
+  };
+   
+}
+const addRandom = guessNumber();
+addRandom();
 
 
-};
-guessNumber();
 
